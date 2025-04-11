ad4e405ef0830b2fc6577b95828c387ac413ad27
Angel Campos <angelandqa@gmail.com>
2025-04-11 18:09:01 +0200

    First commit

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,24 @@
+let edad: number = 25;          //Variable 'edad' de tipo number
+let altura: number = 175.5;     // Variable 'altura' de tipo number
+
+//Operaciones numéricas
+let suma: number = edad + 10;
+let promedio: number = (edad + altura) / 2;
+
+let primerNombre: string = "Juan";  // Variable 'primerNombre' de tipo string
+let apellido: string = 'Lopez';     // Se pueden usar comillas simples
+
+let nombreCompleto: string = primerNombre + " " + apellido // Concatenación de cadenas
+
+let estaActivo: boolean = true;     // Variable 'estaActivo' de tipo boolean
+let estaLogueado: boolean = false;  // Variable 'estaLogueado0 de tipo boolean
+
+// Conicionales
+if (estaActivo) {
+    console.log("El usuario está activo.");
+} else {
+    console.log("El usuario no está activo.")
+}
+
+let numeros: number[] = [1, 2, 3, 4, 5];    // Arreglo de números
+let frutas: string[] = ["apple", ""]
